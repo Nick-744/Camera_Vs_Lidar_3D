693e57f8894d9aa9a3b0071dfff2ec9f34d77c06
G. Nikolaos X. <100804668+Nick-744@users.noreply.github.com>
2025-04-21 09:58:56 +0300

    resnet18/mobilenet

--- my_project_4/deeplab_v3_p/my_model.py
+++ my_project_4/deeplab_v3_p/my_model.py
@@ -1,15 +1,15 @@
-from torchvision.models.segmentation import deeplabv3_mobilenet_v3_large
+from torchvision.models.segmentation import deeplabv3_resnet18
 import torch.nn as nn
 
 # Model - DeepLabV3 με MobileNetV3-Large backbone!
 weights = None
-model = deeplabv3_mobilenet_v3_large(weights = weights)
+model = deeplabv3_resnet18(weights = weights)
 
 # - Θέλουμε 2 κλάσεις:
 # α) ο δρόμος (και οι 2 λωρίδες)
 # β) το υπόλοιπο της εικόνας (εκτός δρόμου)
-model.classifier[4] = nn.Conv2d(256, 2, kernel_size = 1)
-# Οπότε, πρέπει να αντικαταστήσουμε το τελευταίο layer {4} του classifier
+model.classifier[-1] = nn.Conv2d(256, 2, kernel_size = 1)
+# Οπότε, πρέπει να αντικαταστήσουμε το τελευταίο layer {4 ή -1 στο ResNet18/MobileNet} του classifier
 # με 1 Conv2d layer που έχει ΜΟΝΟ 2 εξόδους (classes)!
 # Επίσης, kernel_size = 1 γιατί θέλουμε να κάνουμε pixel-wise classification!
 
